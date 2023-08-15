aa431711ac720194aba838df69fb15a183e9624d
isilkin <isilkin@DEV-COMP15>
2023-08-15 12:11:51 +0300

    development branch created

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,6 +1,18 @@
 const { env } = require('process');
 
-const target = 'https://localhost:5001';
+const target = getLocation();
+
+function getLocation() {
+    console.log('[DBG] Gettings location...')
+        if(this.isGithub()) {
+            return 'https://fb17-95-24-222-254.ngrok-free.app/';
+        } else if ( this.isRemoteWorkspace() ) {
+            return 'https://localhost:443/';
+        } else if ( !this.isNodeJSHost() || this.isNgrok() ) {
+            return `${location.protocol}//${location.host}/`;
+        } else
+            return 'https://localhost:5001/';
+}
 
 const PROXY_CONFIG = [
   {
